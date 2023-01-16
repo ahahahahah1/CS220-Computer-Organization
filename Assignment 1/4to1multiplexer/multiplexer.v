24a080ac52267850aa9e35ef4195f4b21776673c
ahahahahah1 <jakhotiyasiddhant0603@gmail.com>
2023-01-16 16:04:43 +0530

    Changed all codes from behavioural to structural

--- Assignment 1/4to1multiplexer/multiplexer.v
+++ Assignment 1/4to1multiplexer/multiplexer.v
@@ -5,11 +5,16 @@ input i0, i1, i2, i3, s0, s1;
 output wire out;
 wire a1, a2, a3, a4;
 
-assign a1 = (~s0) & (~s1) & i0;
-assign a2 = (s0) & (~s1) & i1;
-assign a3 = (~s0) & (s1) & i2;
-assign a4 = (s0) & (s1) & i3;
+// assign a1 = (~s0) & (~s1) & i0;
+// assign a2 = (s0) & (~s1) & i1;
+// assign a3 = (~s0) & (s1) & i2;
+// assign a4 = (s0) & (s1) & i3;
+and A1(a1, ~s0, ~s1, i0);
+and A2(a2, s0, ~s1, i1);
+and A3(a3, ~s0, s1, i2);
+and A4(a4, s0, s1, i3);
 
-assign out = a1 | a2 | a3 | a4;
+// assign out = a1 | a2 | a3 | a4;
+or o1(out, a1, a2, a3, a4);
 
 endmodule
--- Assignment 1/fullsubtractor/fullsubtractor.v
+++ Assignment 1/fullsubtractor/fullsubtractor.v
@@ -3,12 +3,18 @@ input a, b, c;
 output diff, B_out;
 
 wire diff, B_out;
-wire a1, a2;
+wire a1, a2, xo1;
 
-assign diff = a ^ b ^ c;
+// assign diff = a ^ b ^ c;
+xor x1(diff, a, b, c);
 
-assign a1 = (c & ~(a ^ b));
-assign a2 = (~a & b);
-assign B_out = a1 | a2; 
+// assign a1 = (c & ~(a ^ b));
+// assign a2 = (~a & b);
+// assign B_out = a1 | a2;
+
+xor x2(xo1, a, b);
+and A1(a1, c, ~xo1);
+and A2(a2, ~a, b);
+or o1(B_out, a1, a2);
 
 endmodule
