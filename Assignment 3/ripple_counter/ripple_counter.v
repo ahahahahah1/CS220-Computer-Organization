3dd116b2dcfe3662c69b081f28facfd1d819d403
ahahahahah1 <jakhotiyasiddhant0603@gmail.com>
2023-02-06 18:37:01 +0530

    Code for sequence detection of 1010

--- Assignment 3/ripple_counter/ripple_counter.v
+++ Assignment 3/ripple_counter/ripple_counter.v
@@ -3,28 +3,36 @@
 
 module ripple_counter(clk, flag, reset, q);
 	input clk, flag, reset;
-	output wire [3:0] q;
+	output reg [3:0] q;
 
-	initial begin
-		q = 4'b0;
-	end
+	// initial begin
+	// 	q = 4'b0;
+	// end
 
-	parameter i = 1'b0;
+	// parameter i = 1'b0;
 	// parameter i = flag;
 
-	generate
-		if(i) begin //up-counter
-			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
-			t_flipflop TFF1(.t(1'b1), .clk(q[0]), .q(q[1]), .rst(reset));
-			t_flipflop TFF2(.t(1'b1), .clk(q[1]), .q(q[2]), .rst(reset));
-			t_flipflop TFF3(.t(1'b1), .clk(q[2]), .q(q[3]), .rst(reset));
-		end
-		else begin
-			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
-			t_flipflop TFF1(.t(1'b1), .clk(~q[0]), .q(q[1]), .rst(reset));
-			t_flipflop TFF2(.t(1'b1), .clk(~q[1]), .q(q[2]), .rst(reset));
-			t_flipflop TFF3(.t(1'b1), .clk(~q[2]), .q(q[3]), .rst(reset));
-		end
-	endgenerate
+	// generate
+	// 	genvar i, j;
+	// 	for(i = flag, j = 0; j == 0; j = j + 1) begin: conditional_instantiate
+	// 		if(i) begin //up-counter
+	// 			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
+	// 			t_flipflop TFF1(.t(1'b1), .clk(q[0]), .q(q[1]), .rst(reset));
+	// 			t_flipflop TFF2(.t(1'b1), .clk(q[1]), .q(q[2]), .rst(reset));
+	// 			t_flipflop TFF3(.t(1'b1), .clk(q[2]), .q(q[3]), .rst(reset));
+	// 		end
+	// 		else begin
+	// 			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
+	// 			t_flipflop TFF1(.t(1'b1), .clk(~q[0]), .q(q[1]), .rst(reset));
+	// 			t_flipflop TFF2(.t(1'b1), .clk(~q[1]), .q(q[2]), .rst(reset));
+	// 			t_flipflop TFF3(.t(1'b1), .clk(~q[2]), .q(q[3]), .rst(reset));
+	// 		end
+	// 	end
+	// endgenerate
+  // initiate 4 T-FF to update the count
 
+  t_flipflop tf1(.t(1'b1), .q(q[0]), .clk(clk), .rst(rst));
+  t_flipflop tf2(.t(1'b1), .q(q[1]), .clk(q[0]), .rst(rst));
+  t_flipflop tf3(.t(1'b1), .q(q[2]), .clk(q[1]), .rst(rst));
+  t_flipflop tf4(.t(1'b1), .q(q[3]), .clk(q[2]), .rst(rst));
 endmodule
--- Assignment 3/ripple_counter/ripple_counter_tb.v
+++ Assignment 3/ripple_counter/ripple_counter_tb.v
@@ -1,20 +1,23 @@
-`include "./ripple_counter_top.v"
+`include "./ripple_counter.v"
 
 module ripple_counter_tb;
-	reg flag;
+	reg clk, rst, flag;
 	reg [3:0] q;
 
 	initial begin
-		flag = 1'b0;
-		q <= 4'b0000;
+		rst <= 1'b0;
+		flag <= 1'b0;
+		clk <= 1'b0;
 		forever #200 flag = ~flag;
 	end
+	
+	always #5 clk = ~clk;
 
 	initial begin
 		#1000 $finish;
 	end
 
-	ripple_counter_top RC1(.q(q), .flag(flag));
+	ripple_counter RC1(.clk(clk), .reset(rst), .q(q), .flag(flag));
 
 	always@(*) begin
 		$display("at time = %d, %d %d %d %d", $time, q[3], q[2], q[1], q[0]);
