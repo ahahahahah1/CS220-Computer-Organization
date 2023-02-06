d7ef995127e080a0ad2ba6b3753eab7fc6de8c94
ahahahahah1 <jakhotiyasiddhant0603@gmail.com>
2023-02-06 19:14:09 +0530

    Completed code for for ripple counter using negedge triggered flip flops

--- Assignment 3/ripple_counter/ripple_counter.v
+++ Assignment 3/ripple_counter/ripple_counter.v
@@ -1,38 +1,32 @@
 //this file contains the code for ripple counter
 `include "./t_flipflop.v"
 
-module ripple_counter(clk, flag, reset, q);
-	input clk, flag, reset;
-	output reg [3:0] q;
+// module ripple_counter(clk, flag, reset, q);
+// 	input clk, flag, reset;
+// 	output wire [3:0] q;
+// 	wire clk1, clk2, clk3;
+	
+// 	xor x1(clk1, q[0], control);
+// 	xor x2(clk2, q[1], control);
+// 	xor x3(clk3, q[2], control);
 
-	// initial begin
-	// 	q = 4'b0;
-	// end
+// 	t_flipflop tf1(.t(1'b1), .q(q[0]), .clk(clk), .rst(reset));
+// 	t_flipflop tf2(.t(1'b1), .q(q[1]), .clk(clk1), .rst(reset));
+// 	t_flipflop tf3(.t(1'b1), .q(q[2]), .clk(clk2), .rst(reset));
+// 	t_flipflop tf4(.t(1'b1), .q(q[3]), .clk(clk3), .rst(reset));
+// endmodule
 
-	// parameter i = 1'b0;
-	// parameter i = flag;
+module counter (control, clk, rst, q);
+    input control,clk,rst;
+    output [3:0] q;
 
-	// generate
-	// 	genvar i, j;
-	// 	for(i = flag, j = 0; j == 0; j = j + 1) begin: conditional_instantiate
-	// 		if(i) begin //up-counter
-	// 			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
-	// 			t_flipflop TFF1(.t(1'b1), .clk(q[0]), .q(q[1]), .rst(reset));
-	// 			t_flipflop TFF2(.t(1'b1), .clk(q[1]), .q(q[2]), .rst(reset));
-	// 			t_flipflop TFF3(.t(1'b1), .clk(q[2]), .q(q[3]), .rst(reset));
-	// 		end
-	// 		else begin
-	// 			t_flipflop TFF0(.t(1'b1), .clk(clk), .q(q[0]), .rst(reset));
-	// 			t_flipflop TFF1(.t(1'b1), .clk(~q[0]), .q(q[1]), .rst(reset));
-	// 			t_flipflop TFF2(.t(1'b1), .clk(~q[1]), .q(q[2]), .rst(reset));
-	// 			t_flipflop TFF3(.t(1'b1), .clk(~q[2]), .q(q[3]), .rst(reset));
-	// 		end
-	// 	end
-	// endgenerate
-  // initiate 4 T-FF to update the count
+    wire clk0, clk1, clk2, clk3;
+    xor x1(clk1, control, q[0]);
+    xor x2(clk2, control, q[1]);
+    xor x3(clk3, control, q[2]);
 
-  t_flipflop tf1(.t(1'b1), .q(q[0]), .clk(clk), .rst(rst));
-  t_flipflop tf2(.t(1'b1), .q(q[1]), .clk(q[0]), .rst(rst));
-  t_flipflop tf3(.t(1'b1), .q(q[2]), .clk(q[1]), .rst(rst));
-  t_flipflop tf4(.t(1'b1), .q(q[3]), .clk(q[2]), .rst(rst));
+    tflipflop tff0(q[0], clk, rst);
+    tflipflop tff1(q[1], clk1, rst);
+    tflipflop tff2(q[2], clk2, rst);
+    tflipflop tff3(q[3], clk3, rst);
 endmodule
--- Assignment 3/ripple_counter/t_flipflop.v
+++ Assignment 3/ripple_counter/t_flipflop.v
@@ -1,18 +1,31 @@
 //this file contains the design for a negatively edge triggered t_flipflop
 
-module t_flipflop(t, clk, q, rst);
-	input t, clk, rst;
-	output reg q;
+// module t_flipflop(t, clk, q, rst);
+// 	input t, clk, rst;
+// 	output wire q;
+//   reg d;
 
-	always @(negedge clk) begin
-      if(rst) begin
-        q <= 0;
-      end
-      else begin
-        if(t) begin
-          q <= ~q;
-        end
-      end
-  end
+// 	always @(negedge clk) begin
+//       if(rst) begin
+//         d <= 0;
+//       end
+//       else begin
+//         if(t) begin
+//           d <= ~q;
+//         end
+//       end
+//   end
+
+//   assign q = d;
       
+// endmodule
+
+`include "./d_flipflop.v"
+
+module tflipflop (q,clk,rst);
+    output q;
+    input clk,rst;
+    wire d;
+    dflipflop dff(.q(q),  .d(d), .clk(clk), .rst(rst));
+    not n1(d,q);
 endmodule
