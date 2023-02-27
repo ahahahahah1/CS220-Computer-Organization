e2b2b9895767911174cece0b1d964526b6990668
ahahahahah1 <jakhotiyasiddhant0603@gmail.com>
2023-02-27 17:31:34 +0530

    dekhle bro

--- Assignment 5/fp_adder_top.v
+++ Assignment 5/fp_adder_top.v
@@ -7,39 +7,40 @@
 // if they are different, then the floating point addition is incorrect
 
 module fp_adder_top(
-    input clk,
-    input reset,
-    output reg [31:0] result
+    input switch,
+    output led;
 );
+    // parameter REQ_SUM = 32'b0_1000_0101_1010_1000_0100_0000_0000_000;
 
+    wire [2:0] address;
     reg [31:0] a_fromVEDA;
     reg [31:0] b_fromVEDA;
     reg [31:0] result_fromVEDA;
     reg [31:0] value_fromVEDA;
-    reg [4:0] address_a;
-    reg [4:0] address_b;
-    reg [4:0] address_result;
-    reg mode;
-    reg write_enable;
+    // reg [4:0] address_a;
+    // reg [4:0] address_b;
+    // reg [4:0] address_result;
+    // reg mode;
+    // reg write_enable;
 
     // take random value for a and b
-    wire a_temp = 32'0_0000010_111_1000_0000_0000_0000_0000;
-    wire b_temp = 32'0_0000010_111_1000_0000_0000_0000_0000;
+    // wire a_temp = 32'0_0000010_111_1000_0000_0000_0000_0000;
+    // wire b_temp = 32'0_0000010_111_1000_0000_0000_0000_0000;
     // store floating point addition of a_temp and b_temp in result_temp
-    wire result_temp = 32'0_0000011_000_0000_0000_0000_0000_0000;
+    // wire result_temp = 32'0_0000011_000_0000_0000_0000_0000_0000;
 
     VEDA VEDA1(
         .clk(clk),
-        .reset(reset),
-        .mode(mode),
-        .write_enable(write_enable),
-        .address(address_a),
-        .data_in(a_temp),
+        .reset(0),
+        .mode(1),
+        .write_enable(0),
+        .address(address),
+        .data_in(0),
         .data_out(value_fromVEDA)
     );
 
-    wire [31:0] inp1;
-    wire [31:0] inp2;
+    wire [31:0] inp1 = 32'b0_1000_0101_0111_0101_0000_0000_0000_000;
+    wire [31:0] inp2 = 32'b0_1000_0010_1001_1010_0000_0000_0000_000;
     wire [31:0] out;
 
     fp_adder fp_adder1(
@@ -49,7 +50,19 @@ module fp_adder_top(
     );
 
     always@(*) begin
+        if(switch) begin
+            address = 2;
+        end
+        else begin
+            address = 3;
+        end
 
+        if(value_fromVEDA == out) begin
+            led = 1;
+        end
+        else begin
+            led = 0;
+        end
     end
 
 endmodule
